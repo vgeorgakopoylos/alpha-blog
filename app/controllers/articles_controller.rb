ee017532a602e5efaf86e95999c599c874957e52
vgeorgakopoylos <v.georgakopoylos@gmail.com>
2019-02-23 02:04:44 -0800

    Added show all articles functiionality, added references between pages, deleted cached files and ignored tmp folder contain this type of files

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
   end
@@ -6,7 +11,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params);
     if @article.save
-      flash[:notice] = "Artice was sucecessfully created"
+      flash[:notice] = "Article was sucecessfully created"
       redirect_to article_path(@article)
     else
       render 'new'
@@ -15,6 +20,20 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was sucecessfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
 
   private
