588265af9c10ee0453277efbe12fd01efe8c50bb
vgeorgakopoylos <v.georgakopoylos@gmail.com>
2019-02-23 06:25:00 -0800

    added function set_article in articles_controller for code redundancy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
   def index
     @articles = Article.all
   end
@@ -19,22 +19,9 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
-  end
-
-  def destroy
-    @article = Article.find(params[:id])
-    @article.destroy
-    flash[:notice] = "Article was sucecessfully deleted"
-    redirect_to articles_path
-  end
-
-  def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was sucecessfully updated"
       redirect_to article_path(@article)
@@ -43,7 +30,20 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article.destroy
+    flash[:notice] = "Article was sucecessfully deleted"
+    redirect_to articles_path
+  end
+
+  def edit
+  end
+
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
     def article_params
         params.require(:article).permit(:title, :description)
     end
