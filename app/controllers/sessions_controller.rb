8c97e2c8514043b176ce91924d389c3cafb0d9c4
vgeorgakopoylos <v.georgakopoylos@gmail.com>
2019-03-12 14:43:54 -0700

    added login log out buttons. application helper functions for the site

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have succesfully logged in"
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = "Incorrect Credentials. Please check your email and password"
+      render 'new'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have logged out"
+    redirect_to root_path
   end
 
 end
